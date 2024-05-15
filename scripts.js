9e09fb8cba54b8bed1d3d032ae57d20b54840d6c
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-15 14:30:13 -0400

    Add random color generator and footer

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
+// Add coin flip effect to .project elements
 const projects = document.querySelectorAll('.project'); 
   
 projects.forEach(project => {
   project.addEventListener('mouseenter', function() {
     this.classList.add('animate');
+    project.style.backgroundColor = randomColor();
+    project.style.color = randomColor();
   });
   
   project.addEventListener('mouseleave', function() {
     this.classList.remove('animate');
+    project.style.backgroundColor = '';
+    project.style.color = '';
   });
 });
+
+// Random color generator
+function randomColor() {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return `#${randomColor}`;
+};
+
