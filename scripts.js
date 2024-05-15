bc506cab502627acf4762a5c0feaeee05ac8ef99
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-15 15:28:45 -0400

    Writing code for random colors for the links

--- scripts.js
+++ scripts.js
@@ -1,23 +1,36 @@
 // Add coin flip effect to .project elements
 const projects = document.querySelectorAll('.project'); 
-  
+const navbarLinks = document.querySelectorAll('a');
+
+function randomColor() {  
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return `#${randomColor}`;
+};
+
 projects.forEach(project => {
   project.addEventListener('mouseenter', function() {
     this.classList.add('animate');
     project.style.backgroundColor = randomColor();
-    project.style.color = randomColor();
   });
   
   project.addEventListener('mouseleave', function() {
     this.classList.remove('animate');
     project.style.backgroundColor = '';
-    project.style.color = '';
   });
 });
 
-// Random color generator
-function randomColor() {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return `#${randomColor}`;
-};
+navbarLinks.forEach(link => {
+  let originalColor;
 
+  link.addEventListener('mouseover', () => {
+    originalColor = getComputedStyle(link).color;
+    const color = randomColor();
+    link.style.setProperty('--random-color', color);
+    link.style.color = `var(--random-color)`;
+    link.style.backgroundColor = 'initial';
+  });
+
+  link.addEventListener('mouseleave', () => {
+    link.style.color = originalColor;
+  });
+});
